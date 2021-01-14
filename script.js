a4b4dc9e834ae103752ecde380de3923d8f7adab
Sarah Finck <sarahfinck95@gmail.com>
2021-01-13 18:27:41 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.localStorage
+console.log(localStorage);
+
+//Create a form with 9:00 am - 5:00 pm blocks 
+//According to time expiration my blocks need to change different colors: grey- expired, red- current, green-future
+//When the file is open the current date is displayed
+//When I scroll down I see blocks of times 
+//When I click on a time block I can enter an event 
+//When I save the event it is saved in local storage 
+//When I refresh the page the saved event stays 
