bbab59a53ffdf0005d7c12c38e613d4091671964
Sarah Finck <sarahfinck95@gmail.com>
2021-01-13 20:07:52 -0600

    created divs for elements in CSS

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 window.localStorage
 console.log(localStorage);
 
+var moment = moment(); 
+var timeDiv = $("<div>").att("class","time-block");
+var pastTasks = $("<div>").att("class","past");
+var presentTasks = $("<div>").att("class","present");
+var futureTasks = $("<div>").att("class","future");
+var saveButton = $("<div>").att("class", "saveBtn")
+var userTimes = //create user times array 
+
+
+
+
 //Create a form with 9:00 am - 5:00 pm blocks 
 //According to time expiration my blocks need to change different colors: grey- expired, red- current, green-future
 //When the file is open the current date is displayed
